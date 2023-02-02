743e881f467cec90787fc198e8c7289c62cbf204
Nada Atieh <nada.s.atieh@gmail.com>
2023-02-02 13:28:07 -0600

    Moved the databases section and secret_key into local_settings file

--- songs_project/settings.py
+++ songs_project/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wzy&s5z$a%@g4h^!)a@w6bs(fwe6r0zvtba^yvz#2mmn+z71gn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,18 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'songs_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -124,3 +110,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from songs_project.local_settings import *
+
+except ImportError:
+    pass
